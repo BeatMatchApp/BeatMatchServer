edb50eb9331a5904a2dcd277b94bef6012076bdf
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-04-17 21:04:21 +0300

    change to GeminiParams

--- src/controllers/playlistController.ts
+++ src/controllers/playlistController.ts
@@ -2,9 +2,21 @@ import { Request, Response } from 'express';
 import * as geminiService from '../services/gemini-service';
 import { ApiError } from '../common/errors';
 
+// TODO: if you have an idea to not write interface GeminiParams both on client and server tell me
+export interface GeminiParams {
+    favoriteArtist?: string;
+    mood?: string;
+  }
+
 export const suggestPlaylist = async (req: Request, res: Response): Promise<void> => {
     try {
-        const { favoriteArtist, mood } = req.query;
+        const { geminiParams } = req.query;
+
+        if (!geminiParams ){
+        throw new Error("error while saving")
+        }
+
+        const { favoriteArtist, mood } = geminiParams as GeminiParams;
 
         if (!favoriteArtist && !mood) {
             throw new ApiError(400, 'Please provide either a favorite song or mood.');
