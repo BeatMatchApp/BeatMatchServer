75cf4aef6dfb017a272e10ebd180cbe503526fdf
CarmelBarda <carmelbd78@colman.ac.il>
2025-04-30 13:44:40 +0300

    type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,11 +3,7 @@ import { UserCredentials } from "../../models";
 declare global {
   namespace Express {
     interface Request {
-      userCredentials?: {
-        id: string;
-        spotify_access_token: string;
-        spotify_refresh_token: string;
-      };
+      userCredentials?: UserCredentials;
     }
   }
 }
