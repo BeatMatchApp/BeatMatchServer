5ae189c91c7342959bcf9a6b90c2f539a84421c6
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-05-04 00:18:41 +0300

    authMiddleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,11 @@ import { credentialsMiddleware } from "../middlewares/credentialsMiddleware";
 import playlistRoutes from "./playlistRoutes";
 import { loginHandler } from "../loginHandler";
 import userRoutes from "./userRoutes";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.use("/api/playlist", credentialsMiddleware, playlistRoutes);
+router.use("/api/playlist", credentialsMiddleware, authMiddleware, playlistRoutes);
 router.use("/api/user", userRoutes);
 router.use("/api/login", credentialsMiddleware, loginHandler);
 
--- src/routes/playlistRoutes.ts
+++ src/routes/playlistRoutes.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 import * as playlistController from '../controllers/playlistController';
-import authMiddleware from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.get('/suggestion', authMiddleware, playlistController.suggestPlaylist);
+router.get('/suggestion', playlistController.suggestPlaylist);
 
 export default router;  
