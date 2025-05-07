eefd363ffe742bacbfcc45a8a8dadcc6221362d6
CarmelBarda <carmelbd78@colman.ac.il>
2025-05-07 17:36:16 +0300

    remove spotify shit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { credentialsMiddleware } from "../middlewares/credentialsMiddleware";
 import playlistRoutes from "./playlistRoutes";
 import { loginHandler } from "../loginHandler";
 import userRoutes from "./userRoutes";
@@ -7,8 +6,17 @@ import authMiddleware from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.use("/playlist", credentialsMiddleware, authMiddleware, playlistRoutes);
+router.use(
+  "/playlist",
+  //   spotifyServiceMiddleware,
+  authMiddleware,
+  playlistRoutes
+);
 router.use("/user", userRoutes);
-router.use("/login", credentialsMiddleware, loginHandler);
+router.use(
+  "/login",
+  // spotifyServiceMiddleware,
+  loginHandler
+);
 
 export default router;
