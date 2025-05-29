a85f720c1861ea2305ef31b706869774539950b4
ayatal <ayatal.712@cs.colman.ac.il>
2025-05-29 17:54:43 +0300

    remove dotenv load (add in npm command)

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-console.log("host:", process.env.PG_HOST);
-console.log("password:", process.env.PG_PASSWORD);
 module.exports = {
   development: {
     client: "pg",
