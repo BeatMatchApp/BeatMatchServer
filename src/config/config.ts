2dc5f38de87167003ede151c9536bc9f1fe2c807
CarmelBarda <carmelbd78@colman.ac.il>
2025-05-29 17:14:45 +0300

    port

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  port: parseInt(process.env.PORT || "3000"),
+  port: parseInt(process.env.PORT || "22"),
   prodPort: parseInt(process.env.HTTPS_PORT || "21771"),
   nodeEnv: process.env.NODE_ENV || "development",
   geminiApiKey: process.env.GEMINI_API_KEY,
