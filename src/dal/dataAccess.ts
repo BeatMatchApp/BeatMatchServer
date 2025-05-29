a52b617ef52289178b82d5f15fc3f985a150230a
ayatal <ayatal.712@cs.colman.ac.il>
2025-05-29 17:16:37 +0300

    use process env for db config

--- src/dal/dataAccess.ts
+++ src/dal/dataAccess.ts
@@ -1,7 +1,7 @@
 import { knex } from "knex";
 
 const env = process.env.NODE_ENV || "development";
-const config = require("../config/db");
-const dataAccess = knex(config[env]);
+const dbConfig = require("../config/db");
+const dataAccess = knex(dbConfig[env]);
 
 export { dataAccess };
