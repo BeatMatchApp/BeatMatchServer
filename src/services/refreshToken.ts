f4046b902e2adcbc60b183b01f8858b4ff6e66a0
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-01 16:09:30 +0300

    cookie

--- src/services/refreshToken.ts
+++ src/services/refreshToken.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { refreshSpotifyAccessToken } from "./spotifyService";
 import { createAuthCookie } from "./createAuthCookie";
-import { REFRESH_COOKIE } from "../consts/general";
+import { ACCESS_COOKIE, REFRESH_COOKIE } from "../consts/general";
 
 export const refreshAuthToken = async (req: Request, res: Response) => {
   const refreshToken = req.cookies[REFRESH_COOKIE];
@@ -11,7 +11,7 @@ export const refreshAuthToken = async (req: Request, res: Response) => {
 
     createAuthCookie(res, newAccessToken);
 
-    req.cookies.spotify_access_token = newAccessToken;
+    req.cookies[ACCESS_COOKIE] = newAccessToken;
   } catch (error) {
     res.status(401).json({ error: "Failed to refresh spotify's access token" });
   }
