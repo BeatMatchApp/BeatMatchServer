63f5faa4c2377f8ffa976362e1fd1fefafd98322
ayatal <ayatal.712@cs.colman.ac.il>
2025-06-05 17:47:12 +0300

    get

--- src/routes/spotifyRoutes.ts
+++ src/routes/spotifyRoutes.ts
@@ -6,7 +6,7 @@ import config from '../config/config';
 const router = express.Router();
 
 const getLogin = () => {
-  axios.post(config.spotifyServiceUrl + '/spotifyAPI/login');
+  axios.get(config.spotifyServiceUrl + '/spotifyAPI/login');
 };
 router.use(
   '/login',
