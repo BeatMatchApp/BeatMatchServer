b04b1c8844429912e14ebf57de1269c3518cb389
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-05 18:18:40 +0300

    secure agent

--- src/proxy/spotifyProxy.ts
+++ src/proxy/spotifyProxy.ts
@@ -12,6 +12,7 @@ export class SpotifyProxy extends BaseProxy {
     return this.createProxy({
       target: `${this.getBaseUrl()}/login`,
       pathRewriteBase: '^/spotify/login',
+      secure: false,
     });
   }
 
