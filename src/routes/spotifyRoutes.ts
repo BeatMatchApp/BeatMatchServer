a52ef148164f34fa43b48dfacce88c306b20139a
ayatal <ayatal.712@cs.colman.ac.il>
2025-06-05 17:46:43 +0300

    login

--- src/routes/spotifyRoutes.ts
+++ src/routes/spotifyRoutes.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import { spotifyProxy } from '../proxy/spotifyProxy';
+import axios from 'axios';
+import config from '../config/config';
 
 const router = express.Router();
 
+const getLogin = () => {
+  axios.post(config.spotifyServiceUrl + '/spotifyAPI/login');
+};
 router.use(
   '/login',
   //   (_, _1, next) => {
   //     console.log('Spotify login route hit');
   //     next();
   //   },
-  ...spotifyProxy.createLoginProxy()
+  getLogin
 );
 router.use('/users', ...spotifyProxy.createUsersProxy());
 router.use('/playlists', ...spotifyProxy.createPlaylistsProxy());
