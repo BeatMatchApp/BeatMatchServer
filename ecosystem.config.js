280bfa13bfbf4a868b6043901dc51a7eba92b73e
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-05 14:36:45 +0300

    src

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "server_app",
-      script: "./dist/server.js",
+      script: "./dist/src/app.js",
       env_production: {
         NODE_ENV: "production",
       },
