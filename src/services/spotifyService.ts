80446c74523bf687f5961c1593055baa9078c9a2
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-05 15:57:01 +0300

    lazy load

--- src/services/spotifyService.ts
+++ src/services/spotifyService.ts
@@ -1,27 +1,44 @@
-import axios from "axios";
-import config from "../config/config";
+import axios, { AxiosInstance } from 'axios';
+import config from '../config/config';
 
-export const spotifyService = axios.create({
-  baseURL: config.spotifyServiceUrl,
-  headers: {
-    "Content-type": "application/json",
-  },
-  withCredentials: true,
-});
+let spotifyService: AxiosInstance | null = null;
 
-export const refreshSpotifyAccessToken = async (refreshToken: string) => {
-  try {
-    const response = await spotifyService.post("/spotifyAPI/resfreshToken", {
-      refreshToken,
+function getSpotifyService(): AxiosInstance {
+  if (!spotifyService) {
+    if (!config.spotifyServiceUrl) {
+      throw new Error('spotifyServiceUrl is not defined in config.');
+    }
+
+    spotifyService = axios.create({
+      baseURL: config.spotifyServiceUrl,
+      headers: {
+        'Content-type': 'application/json',
+      },
+      withCredentials: true,
     });
+  }
+
+  return spotifyService;
+}
+
+export const refreshSpotifyAccessToken = async (
+  refreshToken: string
+): Promise<string> => {
+  try {
+    const response = await getSpotifyService().post(
+      '/spotifyAPI/resfreshToken',
+      {
+        refreshToken,
+      }
+    );
 
     if (response.status === 200) {
       return response.data?.accessToken;
     } else {
-      throw new Error("Failed to refresh Spotify access token");
+      throw new Error('Failed to refresh Spotify access token');
     }
   } catch (error) {
-    console.error("Error refreshing Spotify access token:", error);
+    console.error('Error refreshing Spotify access token:', error);
     throw error;
   }
 };
