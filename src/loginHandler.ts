03667eca22a85ae2340894ea08666c62bd2d50e1
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-09 19:53:53 +0300

    fix bug

--- src/loginHandler.ts
+++ src/loginHandler.ts
@@ -1,19 +1,22 @@
-import { Request, Response } from "express";
-import { UsersDAL } from "./dal/users";
+import { Request, Response } from 'express';
+import { UsersDAL } from './dal/users';
 
 export const loginHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  if (req.user) {
-    const user = await UsersDAL.getUserById(req.user.id);
-
-    if (!user) {
-      res.status(401).json({ message: "Unauthorized" });
-      return;
-    }
-
-    res.status(200).json({ user });
+  if (!req.user) {
+    res.status(401).json({ message: 'Unauthorized' });
     return;
   }
+
+  const user = await UsersDAL.getUserById(req.user.id);
+
+  if (!user) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
+  }
+
+  res.status(200).json({ user });
+  return;
 };
